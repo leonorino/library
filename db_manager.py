3da99af0b29986ef0c544bbe5a0c64a64e85b0bb
Владислав Ястребов <vladyastreboc@gmail.com>
2021-04-11 11:50:00 +0700

    Код для запуска приложения + .gitignore

--- db_manager.py
+++ db_manager.py
@@ -3,8 +3,6 @@ import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
-from flask_login import LoginManager, login_user, login_required, logout_user
-from flask_restful import Api
 
 
 database = dec.declarative_base()
