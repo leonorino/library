e0a11fdb039a7f1653037ef195c19b223ad45392
Владислав Ястребов <vladyastreboc@gmail.com>
2021-04-12 21:52:31 +0700

    Добавлена возможность авторизации

--- app.py
+++ app.py
@@ -10,30 +10,22 @@ app.config['SECRET_KEY'] = "secret_key"
 
 
 @app.route("/")
-def main_page():
-    session = db_manager.create_session()
-    books = session.query(Book).all()
-    print(books[0].author)
-    print(books[0].user)
-
-    users = session.query(User).all()
-    print(users[0])
-    print(users[0].added_books)
-    print(users[0].downloaded_books)
+def show_main_page():
+    pass
 
 
 if __name__ == "__main__":
     db_manager.db_init()
 
-    # login_manager = LoginManager()
-    # login_manager.init_app(app)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
-    # from models import user
+    from models import user
 
-    # @login_manager.user_loader
-    # def load_user(user_id):
-    #     session = db_manager.create_session()
-    #     return session.query(user.User).get(user_id)
+    @login_manager.user_loader
+    def load_user(user_id):
+        session = db_manager.create_session()
+        return session.query(user.User).get(user_id)
 
 
     app.run(host="localhost", port=8080)
